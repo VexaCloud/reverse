795a893cae0de9aca47db60b19df107a3902f8d7
VexaCloud <orrysander@gmail.com>
2026-03-14 17:51:15 -0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,15 @@
+//path is /?url="the url you want to visit!"
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    //if you are forking this repo and want to change the link, change the link below this line!
-    const TARGET = "https://gym.global.ssl.fastly.net/";
-    const upstream = new URL(TARGET + url.pathname + url.search);
+    const target = url.searchParams.get("url");
+
+    if (!target) {
+      return new Response("Missing ?url=", { status: 400 });
+    }
+
+    const upstream = new URL(target);
+
     const newRequest = new Request(upstream, {
       method: request.method,
       headers: request.headers,
@@ -12,25 +18,36 @@ export default {
         : undefined,
       redirect: "manual"
     });
+
     let response = await fetch(newRequest);
     const newHeaders = new Headers(response.headers);
+
     newHeaders.delete("content-security-policy");
     newHeaders.delete("x-frame-options");
-    newHeaders.delete("x-content-type-options");
     newHeaders.delete("referrer-policy");
+
     const contentType = newHeaders.get("content-type") || "";
+
     if (contentType.includes("text/html")) {
       let text = await response.text();
-      text = text
-        .replace(/https:\/\/proxyium\.com/gi, "")
-        .replace(/href="\//gi, 'href="')
-        .replace(/src="\//gi, 'src="');
+
+      const blocker = `
+<script>
+(function(){
+  const stay=()=>{};
+  window.open=stay;
+  window.top!==window && (window.top.location=window.location);
+})();
+</script>`;
+
+      text = text.replace(/<\/body>/i, blocker + "</body>");
 
       return new Response(text, {
         status: response.status,
         headers: newHeaders
       });
     }
+
     return new Response(response.body, {
       status: response.status,
       headers: newHeaders
